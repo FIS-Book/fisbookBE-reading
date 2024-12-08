efbdf36989af35d70eb1c8184312e42f0970d9a8
Jeusaipakoimetre <mathis.goujon27@gmail.com>
2024-12-08 17:42:58 +0100

    Added Unit Test Wrote unit test for api and entity Created test class for controller Wrote some api and entity getter and setter explicitely

--- src/main/java/us/es/reading/api/EmailRequest.java
+++ src/main/java/us/es/reading/api/EmailRequest.java
@@ -1,15 +1,30 @@
 package us.es.reading.api;
 
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
 @NoArgsConstructor
 public class EmailRequest {
 
     private String to;
     private String subject;
-    private String body;    
+    private String body;
+
+    public String getTo() {
+        return to;
+    }
+    public void setTo(String to) {
+        this.to = to;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    public String getBody() {
+        return body;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }    
 }
--- src/main/java/us/es/reading/entity/ReadingEntity.java
+++ src/main/java/us/es/reading/entity/ReadingEntity.java
@@ -6,12 +6,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @Document(value = "readings")
-@Getter
-@Setter
 public class ReadingEntity {
 
     @Id
@@ -20,4 +15,30 @@ public class ReadingEntity {
     private String userId;
 
     List<Genre> genres;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    
 }
