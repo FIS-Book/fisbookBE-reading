6acac8df1bfb1f801ec0ee42773403c1b6366a00
Edwin Arévalo <edwinarevaloangulo@gmail.com>
2025-01-03 10:20:35 +0100

    Añado variable de entorno basic_url

--- src/main/java/us/es/reading/config/SwaggerConfig.java
+++ src/main/java/us/es/reading/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package us.es.reading.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,8 +9,10 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
-public class SwaggerConfig {        
-
+public class SwaggerConfig { 
+        
+        @Value("${base.url:http://localhost:3000}")
+        private String swaggerUrl;
         @Bean
         public OpenAPI customOpenAPI() {
                 // Configurar Swagger para usar la base URL personalizada
@@ -18,7 +21,7 @@ public class SwaggerConfig {
                                                 .title("Reading Service API")
                                                 .version("v1")
                                                 .description("API Documentation for Reading Service"))
-                                .addServersItem(new Server().url("") // Aquí solo usamos apiVersion, no agregamos
+                                .addServersItem(new Server().url(swaggerUrl) // Aquí solo usamos apiVersion, no agregamos
                                                                      // "/readings" ni otras rutas adicionales
                                                 .description("Base URL for Reading Service"));
         }
