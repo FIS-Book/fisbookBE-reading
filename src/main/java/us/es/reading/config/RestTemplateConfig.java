3d6831420344ccc568b415552bca9bf24e01201d
Edwin Arévalo <edwinarevaloangulo@gmail.com>
2025-01-06 04:02:30 +0100

    Comunicación entre microservicios de catálogos, tanto para agregar libros, eliminar libros y eliminar listados de generos de libros

---
+++ src/main/java/us/es/reading/config/RestTemplateConfig.java
@@ -0,0 +1,10 @@
+package us.es.reading.config;
+
+// @Configuration
+public class RestTemplateConfig {
+
+//     @Bean
+//     public RestTemplate restTemplate() {
+//         return new RestTemplate();
+//     }
+}
